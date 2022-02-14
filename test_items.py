e6a723eaa371c9f85a650bb3164038ef6fa91c95
Sergey <snmakarenkoff@gmail.com>
2022-02-14 19:19:16 +0300

    Added message to assert

--- test_items.py
+++ test_items.py
@@ -5,4 +5,4 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_find_button_add_to_basket(browser):
     browser.get(link)
     time.sleep(5)
-    assert browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert browser.find_element_by_css_selector(".btn-add-to-basket"), "Button not found on page"
